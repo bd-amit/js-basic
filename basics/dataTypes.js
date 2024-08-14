1982edc2af350105ca035de00713da72a5a4af92
bd-amit <amit.gupta20194@gmail.com>
2024-08-14 08:59:14 +0000

    dataTypes js modified

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -19,3 +19,35 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+
+//  Primitive -> Call by value -> Stored in Stack
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedOut = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+// Reference (Non primitive) -> call by reference -> Stored in heap
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Amit",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
